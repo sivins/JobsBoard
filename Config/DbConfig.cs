c117d140340a181ad7488e3d4ba8cd074e51c5d0
sivins <sean.ivins@yahoo.com>
2017-07-01 15:03:04 -0500

    House cleaning

---
+++ Config/DbConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace reverseJobsBoard.Config
+{
+
+    public class TestConfigs
+    {
+
+        public string testDBUrl = "Server=tcp:dashcraftestserver.database.windows.net,1433;Initial Catalog=testDB;Persist Security Info=False;User ID=testdb;Password=Help1022;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+
+
+    }
+
+
+}
